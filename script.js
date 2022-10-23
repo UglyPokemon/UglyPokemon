bd780e26e232df260f23611d5954526ea3901122
Jie eun Lee <jieeunlee91@gmail.com>
2022-10-23 14:40:34 -0400

    Finished input result to show image in container after button click

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 const app = {};
 
-const pokemon = "charizard";
-
-//next step is to conenct input box from HTML to our API call using 'pokemon' const
-
 const baseUrl = "https://pokeapi.co/api/v2/pokemon/";
 
+// grab button from index.html
+const button = document.getElementById("getPokemon");
+
+
 app.init = () => {
-  app.getApp();
+  // add click event to the button 
+  button.addEventListener('click', e => {
+  // grab input from index.html  
+  const pokemonInput = document.getElementById("pokemonInput");
+  // print search result from input value
+    app.getApp(pokemonInput.value);
+  })
 };
 
-app.getApp = () => {
+app.getApp = (pokemon) => {
+
   fetch(`${baseUrl}${pokemon}`)
     .then((res) => {
       console.log(res);
@@ -24,6 +31,8 @@ app.getApp = () => {
       const imgSrc = pokemonInfo.sprites.front_default;
       const imgContainer = document.querySelector(".imageContainer");
       const img = document.createElement("img");
+      // clear inside image container 
+      imgContainer.innerHTML = ``;
       img.src = imgSrc;
       img.alt = `picture of ${pokemonInfo.name}`;
       imgContainer.appendChild(img);
@@ -31,7 +40,7 @@ app.getApp = () => {
     .catch((error) => {
       console.log(error);
       if (error.message === "Not Found") {
-        alert("pick a pokemon that exists! use a new number or name.");
+        alert("pick a pokemon that exists! input the correct pokemon name.");
       } else {
         alert("something is wrong");
       }
